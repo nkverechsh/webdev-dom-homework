efc83acb737b460fb158f6d674f2066e301fa080
nkverechsh <vereshagin-n@mail.ru>
2023-07-21 15:46:45 +0300

    correction main

--- main.js
+++ main.js
@@ -33,39 +33,39 @@ function apiGetStartPage() {
 
 apiGetStartPage();
 
-function apiGet() {
-    isLoadingAllComments = true;
-    render.renderUserComments({userComment, isLoading, addComment});
-    api.getFetch()
-    .then((responseData) => {
-        isLoading = false;
-        isLoadingAllComments = false;
-        userComment = responseData.comments.map((comment) => {
-            let date = new Date(comment.date);
-            date = time.formatCommentDate(date);
-            return {
-                id: comment.id,
-                name: comment.author.name,
-                date: date,
-                comment: comment.text,
-                likes: comment.likes,
-                isLike: true,
-                isEdit: false,
-            };
-        })
-        render.renderUserComments({userComment, isLoading, addComment});
-    })
-        .catch((error) => {
-            api.catchFetch(error);
-        })
-};
+// function apiGet() {
+//     isLoadingAllComments = true;
+//     render.renderUserComments({userComment, isLoading, addComment});
+//     api.getFetch()
+//     .then((responseData) => {
+//         isLoading = false;
+//         isLoadingAllComments = false;
+//         userComment = responseData.comments.map((comment) => {
+//             let date = new Date(comment.date);
+//             date = time.formatCommentDate(date);
+//             return {
+//                 id: comment.id,
+//                 name: comment.author.name,
+//                 date: date,
+//                 comment: comment.text,
+//                 likes: comment.likes,
+//                 isLike: true,
+//                 isEdit: false,
+//             };
+//         })
+//         render.renderUserComments({userComment, isLoading, addComment});
+//     })
+//         .catch((error) => {
+//             api.catchFetch(error);
+//         })
+// };
 
 function addComment() {
     isLoading = true;
     api.postFetch()
     .then(() => {
         render.renderUserComments({userComment, isLoading, addComment,});
-        return apiGet();
+        return apiGetStartPage();
     })
         .catch((error) => {
             api.catchFetch(error);
